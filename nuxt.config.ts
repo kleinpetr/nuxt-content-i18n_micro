ab817f197d218fc7c903fe2a8b65bc2bf227000b
Klein Petr <petr@kleinpetr.com>
2024-10-08 10:09:26 +0200

    feat: reproduce error

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: ['@nuxt/content', 'nuxt-i18n-micro'],
+  i18n: {
+    locales: [
+      { code: 'en', iso: 'en-US', dir: 'ltr' },
+      { code: 'cs', iso: 'cs-CZ', dir: 'ltr' },
+    ],
+    defaultLocale: 'cs',
+    translationDir: 'locales',
+    meta: false,
+    disablePageLocales: true,
+  },
+})
